02937fbdc784a73ac6c19db32c3d2b9ee202f309
Blekas10 <nojus.stankevicius@gmail.com>
2022-03-22 12:33:42 +0200

    paruostas tapk partneriu page

--- tapk-partneriu.php
+++ tapk-partneriu.php
@@ -5,13 +5,13 @@
 <div class="container justify-content-center p-5">
   <div class="row">
 
-    <div class="col-sm-7">
+    <div class="col-lg-7">
       <div class="image-wrapper position-relative h-100 w-100">
         <div class="levander-img"></div>
 
       </div>
     </div>
-    <div class="col-sm-5 py-5">
+    <div class="col-lg-5 py-5">
       <h2 class="sustainable-products">
         WHY CHOOSE MY.ORGANICS?
       </h2>
@@ -30,32 +30,42 @@
   </div>
 </div>
 
-<div class="col-sm-12 text-center ">
-  <div class="tapk-partneriu">
-    <h3>Be the difference your clients want to see and find out how My.Organics can help your salon and the planet. Simply fill out the form below.</h3>
-  </div>
 
+
+<div class="container text-center tapk-partneriu">
+  <div class="row">
+    <div class="col-sm-12 ">
+      <p class="mb-3">Be the difference your clients want to see and find out how My.Organics can help your salon and the planet. Simply fill out the form below.</p>
+
+    </div>
+  </div>
 </div>
 
 
-<div class="container">
+
+<div class="container pt-2 pb-5">
   <form action="action_page.php">
 
-    <label for="fname">First Name</label>
-    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+    <label for="fname">Vardas</label>
+    <input type="text" id="fname" name="firstname" placeholder="Jūsų vardas">
 
-    <label for="lname">Last Name</label>
-    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
-
-    <label for="country">Country</label>
+    <label for="country">Užimamos pareigos </label>
     <select id="country" name="country">
-      <option value="australia">Australia</option>
+      <option value="australia">Kirpėjas</option>
       <option value="canada">Canada</option>
       <option value="usa">USA</option>
     </select>
 
-    <label for="subject">Subject</label>
-    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+    <label for="lname">El.Pašto adresas</label>
+    <input type="text" id="lname" name="lastname" placeholder="El.pašto adresas">
+
+    <label for="lname">Tel. Numeris</label>
+    <input type="text" id="lname" name="lastname" placeholder="Tel. Numeris">
+
+
+
+    <label for="subject">Jūsų žinutė</label>
+    <textarea id="subject" name="subject" placeholder="Parašykite kažką"></textarea>
 
     <input type="submit" value="Submit">
 
