44e8b83253571c816a6e2358c43081367ffe2f51
Blekas10 <nojus.stankevicius@gmail.com>
2022-03-22 11:19:04 +0200

    paruostas duk

--- DUK.php
+++ DUK.php
@@ -1,6 +1,10 @@
 <?php include_once 'header.php' ?>
 
-<div class="container">
+<div class="container duk">
+  <h2>ABOUT OUR PRODUCTS</h2>
+
+
+
   <button class="accordion">Section 1</button>
   <div class="panel">
     <p>Lorem ipsum...</p>
@@ -11,11 +15,41 @@
     <p>Lorem ipsum...</p>
   </div>
 
+
+  <h2>ABOUT OUR PRODUCTS</h2>
+
+
+
+
   <button class="accordion">Section 3</button>
   <div class="panel">
     <p>Lorem ipsum...</p>
   </div>
+
+  <button class="accordion">Section 1</button>
+  <div class="panel">
+    <p>Lorem ipsum...</p>
+  </div>
+
+  <button class="accordion">Section 2</button>
+  <div class="panel">
+    <p>Lorem ipsum...</p>
+  </div>
+
+  <button class="accordion">Section 1</button>
+  <div class="panel">
+    <p>Lorem ipsum...</p>
+  </div>
+
+  <button class="accordion">Section 2</button>
+  <div class="panel">
+    <p>Lorem ipsum...</p>
+  </div>
+
+
+
 </div>
+
 
 
 
