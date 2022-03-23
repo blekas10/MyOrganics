d3c01f35c81e897e506919c9da687ad34ccfdb00
Blekas10 <nojus.stankevicius@gmail.com>
2022-03-23 13:07:41 +0200

    sutvarkytas dazai

--- dazai.php
+++ dazai.php
@@ -1,44 +1,155 @@
 <?php include_once 'header.php' ?>
 
-
-<div class="hero-carousel">
-  <div class="owl-carousel owl-theme">
-    <div class="dazai-hero-image">
-      <div class="container text-center">
-        <div class="dazai-hero-text">
-          <h1 class="dazai-uppercase">Be green be inovated</h1>
-          <p>And I'm a front-End Developer</p>
-
-
-        </div>
-      </div>
-    </div>
-
-    <div class="dazai-hero-image">
-      <div class="dazai-hero-text">
-        <h1 class="dazai-uppercase">Be green be inovated</h1>
-        <p>And I'm a front-End Developer</p>
-
-
-      </div>
-    </div>
-    <div class="dazai-hero-image">
-      <div class="dazai-hero-text">
-        <h1 class="dazai-uppercase">Be green be inovated</h1>
-        <p>And I'm a front-End Developer</p>
-
-
-      </div>
-    </div>
+<div class="container-fluid">
+  <div class="dazai-hero-img">
   </div>
 </div>
 
 <div class="container dazai-prisistatymas py-5 text-center">
   <div class="row">
     <div class="col-lg-12">
-      <p class="mb-4">DISCOVER COLORGANICS</p>
-      <p>Colorganics is the smart colour choice for salons and freelance hairdressers who strive to wow their eco aware clients with premium results.</p>
-      <p>The inter-mixable range of permanent and semi-permanent shades and toners use nature’s finest active ingredients for consistent, damage-free results you and your clients will love.</p>
+      <p class="mb-4 prisistatymas1">DISCOVER COLORGANICS</p>
+      <p class="prisistatymas2">Colorganics is the smart colour choice for salons and freelance hairdressers who strive to wow their eco aware clients with premium results.</p>
+      <p class="prisistatymas2">The inter-mixable range of permanent and semi-permanent shades and toners use nature’s finest active ingredients for consistent, damage-free results you and your clients will love.</p>
+    </div>
+  </div>
+</div>
+
+<div class="container justify-content-center p-5">
+  <div class="row">
+
+
+    <div class="col-lg-6 py-5">
+      <div class="container dazai-text ">
+        <h2 class="sustainable-products">
+          WHY CHOOSE MY.ORGANICS?
+        </h2>
+        <p>
+          61% of millennials say they would pay more for eco-friendly products*
+        </p>
+        <!-- <strong>7 High-Performing Care and Styling Ranges</strong> -->
+        <p>
+          Specially formulated ranges to solve all your clients’ hair and scalp complaints: Hydrating, Thickening, Purifying, Restructuring, After Colour, Resurrection and Keratin Lines.
+        </p>
+        <!-- <strong>70 Shade Certified Organic Colour Line</strong> -->
+        <p>
+          42% of consumers are looking for certification logos to help them choose organic**
+        </p>
+      </div>
+
+    </div>
+
+    <div class="col-lg-6">
+      <div class="image-wrapper ">
+        <div>
+          <img src="images/banish_grey_model_720x.jpg" class="dazai-img">
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div class="container justify-content-center p-5">
+  <div class="row">
+
+    <div class="col-lg-6">
+      <div class="image-wrapper ">
+        <div>
+          <img src="images\14_09_COLORGANICS_720x.jpg" class="dazai-img1">
+        </div>
+
+      </div>
+    </div>
+
+    <div class="col-lg-6 py-5">
+      <div class="container dazai-text1 ">
+        <h2 class="sustainable-products">
+          WHY CHOOSE MY.ORGANICS?
+        </h2>
+        <p>
+          61% of millennials say they would pay more for eco-friendly products*
+        </p>
+        <!-- <strong>7 High-Performing Care and Styling Ranges</strong> -->
+        <p>
+          Specially formulated ranges to solve all your clients’ hair and scalp complaints: Hydrating, Thickening, Purifying, Restructuring, After Colour, Resurrection and Keratin Lines.
+        </p>
+        <!-- <strong>70 Shade Certified Organic Colour Line</strong> -->
+        <p>
+          42% of consumers are looking for certification logos to help them choose organic**
+        </p>
+      </div>
+
+    </div>
+
+  </div>
+</div>
+
+<div class="container text-center py-3 dazai-siulome">
+  <p>COLORGANICS SALONS ALSO LOVE</p>
+</div>
+<div class="container">
+  <div class="row d-flex justify-content-center">
+
+    <div class=" col-md-4 d-flex text-center justify-content-around">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="images/shade_chart_540x.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary btn-dazai">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4 text-center d-flex justify-content-around">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="images/b24c9fa0-7b8d-4270-a745-2a68f54aa41e_540x.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary btn-dazai">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4 d-flex text-center justify-content-around">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="images/Colour_protect_540x.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary btn-dazai">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div class="container-fluid">
+  <div class="dazai-hero-img">
+    <div class="container">
+      <div class="row">
+
+        <div class=" col-md-4 ">
+          <div class="card p-0 kortele-dazai" style="width: 21rem;">
+
+            <div class="card-body">
+              <h5 class="card-title">OUR PROFESSIONAL PROMISE</h5>
+              <p class="card-text">Colorganics organic hair colour is made by hairdressers for hairdressers. We will never knowingly sell Colorganics to consumers and we’ll always offer our very best price to hairstylists - so you won’t be undersold by big-name retailers.</p>
+              <p class="card-text">Colorganics organic hair colour is made by hairdressers for hairdressers. We will never knowingly sell Colorganics to consumers and we’ll always offer our very best price to hairstylists - so you won’t be undersold by big-name retailers.</p>
+              <a href="#" class="btn btn-primary btn-dazai">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+
+        <div class=" col-md-4 "></div>
+        <div class=" col-md-4 "></div>
+
+
+      </div>
     </div>
   </div>
 </div>
