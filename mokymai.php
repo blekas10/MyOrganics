cc7da238c0ba7e1c42769d4fa0486105aa7473eb
Blekas10 <nojus.stankevicius@gmail.com>
2022-03-24 13:18:12 +0200

    sustilizuotas tapk partn. ir mokymai, idetos tos pacios formos

--- mokymai.php
+++ mokymai.php
@@ -17,60 +17,81 @@
 </div>
 
 
-<div class="container text-center mokymai-jums">
-
-  <div class="col-m-12">
-    <h2>MY.ORGANICS TRAINING CREATED JUST FOR YOU</h2>
-    <p>You and your clients deserve the best. We offer the masterclasses and workshops to make you the best organic colourist and stylist you can be.</p>
-  </div>
-</div>
-
-
-<div class="container py-5">
+<div class="container dazai-prisistatymas py-5 text-center">
   <div class="row">
-    <div class="col-lg-6 pb-5 ">
-      <div class="container d-flex justify-content-around">
-        <div class="mokymai-img "></div>
-      </div>
-
-    </div>
-    <div class="col-lg-5">
-      <h4>COLORGANICS ORGANIC HAIR COLOUR TRAINING</h4>
-      <p>Become an expert organic colourist with our specialist Colorganics training. Join us for a Colorganics Masterclass or request exclusive in-salon training.</p>
-      <p>Your My.Educator will demonstrate:</p>
-      <ul>
-        <li>labas litras labas litras</li>
-        <li>labas litras labas litras</li>
-        <li>labas litras labas litras</li>
-        <li>labas litras labas litras</li>
-        <li>labas litras labas litras</li>
-        <li>labas litras labas litras</li>
-      </ul>
+    <div class="col-lg-12">
+      <p class="mb-4 prisistatymas1">DISCOVER COLORGANICS</p>
+      <p class="prisistatymas2">Colorganics is the smart colour choice for salons and freelance hairdressers who strive to wow their eco aware clients with premium results.</p>
+      <p class="prisistatymas2">The inter-mixable range of permanent and semi-permanent shades and toners use nature’s finest active ingredients for consistent, damage-free results you and your clients will love.</p>
     </div>
   </div>
 </div>
 
-<div class="container py-5">
+
+<div class="justify-content-center p-5 ">
   <div class="row">
-    <div class="col-lg-5">
-      <div class="container ml-5">
-        <h4>COLORGANICS ORGANIC HAIR COLOUR TRAINING</h4>
-        <p>Become an expert organic colourist with our specialist Colorganics training. Join us for a Colorganics Masterclass or request exclusive in-salon training.</p>
-        <p>Your My.Educator will demonstrate:</p>
-        <ul>
-          <li>labas litras labas litras</li>
-          <li>labas litras labas litras</li>
-          <li>labas litras labas litras</li>
-          <li>labas litras labas litras</li>
-          <li>labas litras labas litras</li>
-          <li>labas litras labas litras</li>
-        </ul>
+
+
+    <div class="col-lg-6 py-5 mokymai-foto">
+      <div class="container dazai-text ">
+        <h2 class="sustainable-products">
+          WHY CHOOSE MY.ORGANICS?
+        </h2>
+        <p>
+          61% of millennials say they would pay more for eco-friendly products*
+        </p>
+        <!-- <strong>7 High-Performing Care and Styling Ranges</strong> -->
+        <p>
+          Specially formulated ranges to solve all your clients’ hair and scalp complaints: Hydrating, Thickening, Purifying, Restructuring, After Colour, Resurrection and Keratin Lines.
+        </p>
+        <!-- <strong>70 Shade Certified Organic Colour Line</strong> -->
+        <p>
+          42% of consumers are looking for certification logos to help them choose organic**
+        </p>
       </div>
 
     </div>
-    <div class="col-lg-6 pb-5 ">
-      <div class="container d-flex justify-content-center">
-        <div class="mokymai-img "></div>
+
+    <div class="col-lg-6">
+      <div class="image-wrapper ">
+        <div>
+          <img src="images/shade_chart_540x.jpg" class="mokymai-img">
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div class="justify-content-center p-5">
+  <div class="row">
+
+    <div class="col-lg-6 mokymai-foto">
+      <div class="image-wrapper ">
+        <div>
+          <img src="images/Product_training_720x.jpg" class="dazai-img1">
+        </div>
+
+      </div>
+    </div>
+
+    <div class="col-lg-6 py-5">
+      <div class="container dazai-text1 ">
+        <h2 class="sustainable-products">
+          WHY CHOOSE MY.ORGANICS?
+        </h2>
+        <p>
+          61% of millennials say they would pay more for eco-friendly products*
+        </p>
+        <!-- <strong>7 High-Performing Care and Styling Ranges</strong> -->
+        <p>
+          Specially formulated ranges to solve all your clients’ hair and scalp complaints: Hydrating, Thickening, Purifying, Restructuring, After Colour, Resurrection and Keratin Lines.
+        </p>
+        <!-- <strong>70 Shade Certified Organic Colour Line</strong> -->
+        <p>
+          42% of consumers are looking for certification logos to help them choose organic**
+        </p>
       </div>
 
     </div>
@@ -78,35 +99,47 @@
   </div>
 </div>
 
-<div class="container text-center mano-komanda pb-5">
-  <h2>MY.TEAM</h2>
-  <p>New for 2022 My.Organics is celebrating the most talented, creative hairdressers who share our mission to make the world a
-    better place one eco product at a time.
-
-    Watch this space….</p>
-</div>
-
-
-<div class="container">
-  <div class="contact-clean">
-    <form onsubmit="return false;">
-      <h2 class="text-center">Contact us</h2>
-
-      <div class="row input-row">
-        <div class="col-sm-6">
-          <div class="form-group"><input class="form-control" type="text" name="name" placeholder="Name"></div>
-        </div>
-        <div class="col-sm-6">
-          <div class="form-group1"><input class="form-control" type="email" name="email" placeholder="Email"></div>
-        </div>
-      </div>
-
-
-
-      <div class="form-group"><textarea class="form-control" name="message" placeholder="Message" rows="14"></textarea></div>
-      <div class="form-group text-center pt-5"><button class="btn btn-dark" type="submit">send </button></div>
-    </form>
+<div class="container dazai-prisistatymas py-5 text-center">
+  <div class="row">
+    <div class="col-lg-12">
+      <p class="mb-4 prisistatymas1">MY.TEAM</p>
+      <p class="prisistatymas2">New for 2022 My.Organics is celebrating the most talented, creative hairdressers who share our mission to make the world a
+        better place one eco product at a time.</p>
+      <p class="prisistatymas2">Watch this space….</p>
+    </div>
   </div>
 </div>
+
+
+
+
+<div class="container mb-5">
+  <form action="action_page.php">
+
+
+    <div class="row">
+      <div class="col-md-6"><label for="fname">Vardas</label>
+        <input type="text" id="fname" name="firstname" placeholder="Jūsų vardas">
+      </div>
+      <div class="col-md-6"><label for="lname">El.Pašto adresas</label>
+        <input type="text" id="lname" name="lastname" placeholder="El.pašto adresas">
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+    <label for="subject">Jūsų žinutė</label>
+    <textarea id="subject" name="subject" placeholder="Parašykite kažką"></textarea>
+    <div class="text-center mokymai-btn"><input type="submit" value="Submit"></div>
+
+
+  </form>
+</div>
+
 
 <?php include_once 'footer.php' ?>
